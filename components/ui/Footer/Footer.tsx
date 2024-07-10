44248357dd5c7c8c279000f8f60260373ddefa86
Javier Pallarés <90272125+bale2manos@users.noreply.github.com>
2024-07-10 09:47:15 +0200

    ZUISO-8: link to 'about' in footer

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -30,7 +30,7 @@ export default function Footer() {
             </li>
             <li className="py-3 md:py-0 md:pb-4">
               <Link
-                href="/"
+                href="/about"
                 className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
               >
                 About
