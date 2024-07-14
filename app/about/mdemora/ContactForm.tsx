654674592456e8ddc1a5cbd85b859e3d758f918b
bale2 <bale2manos@gmail.com>
2024-07-14 13:16:29 +0200

    el boton este pesado

--- app/about/mdemora/ContactForm.tsx
+++ app/about/mdemora/ContactForm.tsx
@@ -21,7 +21,7 @@ import {
   SelectTrigger,
   SelectValue
 } from '@/components/ui/select';
-import { Button } from '@/components/ui/Button';
+import { Button } from '@/components/ui/button';
 
 // definimos la validacion y el esquema, automaticamente lo tipa
 const formSchema = z.object({
