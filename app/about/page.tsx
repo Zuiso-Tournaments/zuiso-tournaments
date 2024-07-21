301a7e8c4970b6d3801a2775dfc894ea14c26ffb
bale2 <bale2manos@gmail.com>
2024-07-21 16:11:52 +0200

    About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,15 @@
+import AboutList from '@/app/about/AboutList';
+
+
 export default async function AboutPage() {
   return (
     <section className="mb-32 bg-black">
-      <div className="max-w-6xl px-4 py-8 mx-auto sm:px-6 sm:pt-24 lg:px-8">
+      <div className="max-w-6xl px-4 mx-auto sm:px-6 lg:px-8">
         <div className="sm:align-center sm:flex sm:flex-col">
-          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
+          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl pb-8">
             About
           </h1>
+          <AboutList  />
         </div>
       </div>
     </section>
