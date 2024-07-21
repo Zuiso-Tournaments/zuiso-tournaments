9eeea7d66f1de14c9dea27b144165095c41f7dac
garmo17 <marcosgarmo@gmail.com>
2024-07-21 15:31:07 +0200

    Cambio del link al de next

--- app/about/Marcos/SongList.tsx
+++ app/about/Marcos/SongList.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
 
 export interface Song {
   id: string;
@@ -34,7 +35,7 @@ const SongList = () => {
                   <li key={song.id} className="mb-2 p-4 border border-gray-300 rounded shadow hover:border-4 hover:border-gray-700">
                       <h2 className="text-xl font-semibold">{song.title}</h2>
                       <p className="text-gray-600">{song.artista}</p>
-                      <a href={`/about/Marcos/music/${song.id}`} className="text-blue-500 hover:text-blue-700 underline">View song</a>
+                      <Link href={`/about/Marcos/music/${song.id}`} className="text-blue-500 hover:text-blue-700 underline">View song</Link>
                   </li>
               ))}
           </ul>
