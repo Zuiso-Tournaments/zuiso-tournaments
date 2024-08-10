7549c58bf8527d90c4880f2a541e665c759b3098
garmo17 <marcosgarmo@gmail.com>
2024-08-10 14:23:54 +0200

    Hook de feedback para casos de operaciones exitosas y con errores

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import {ReactQueryClientProvider} from '@/components/ReactQueryClientProvider';
 import Footer from '@/components/ui/Footer';
 import Navbar from '@/components/ui/Navbar';
-import {Toaster} from '@/components/ui/Toasts/toaster';
+import { Toaster } from "@/components/ui/toaster"
 import {getURL} from '@/lib/helpers';
 import {Provider} from 'jotai';
 import type {Metadata} from 'next';
