9d392f44abca2f73b64622b77783655ecd6f716c
MdeMora <miguel.uni.demora@gmail.com>
2024-08-14 18:51:35 +0200

    ZUISO-XXXX: fix build

--- app/about/Marcos/TODO/page.tsx
+++ app/about/Marcos/TODO/page.tsx
@@ -1,27 +1,30 @@
 'use client';
-import { Button } from '@/components/ui/button';
+
+import {TodoCard} from '@/app/about/marcos/TODO/TodoCard';
+import {Button} from '@/components/ui/button';
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormMessage,
-  FormDescription,
 } from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-import { useState, useEffect } from 'react'; 
-import type { Tarea, TareaInsert } from '@/db/schemas/tabla_marcos';
-import { TodoCard } from "@/app/about/Marcos/TODO/TodoCard";
+import {Input} from '@/components/ui/input';
+import type {Tarea, TareaInsert} from '@/db/schemas/tabla_marcos';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {useEffect, useState} from 'react';
+import {useForm} from 'react-hook-form';
+import {z} from 'zod';
 
 const formSchema = z.object({
   task: z.string().nonempty(),
 });
 
 export default function TODO() {
-  const [submitStatus, setSubmitStatus] = useState<'idle' | 'success' | 'error'>('idle');
+  const [submitStatus, setSubmitStatus] = useState<
+    'idle' | 'success' | 'error'
+  >('idle');
   const [tasks, setTasks] = useState<Tarea[]>([]);
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -46,7 +49,7 @@ export default function TODO() {
   }
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
-    const task: TareaInsert = { tarea: values.task };
+    const task: TareaInsert = {tarea: values.task};
     try {
       const response = await fetch('/api/MarcosApiTask', {
         method: 'POST',
@@ -78,16 +81,16 @@ export default function TODO() {
   async function handleCheckboxChange(taskId: number) {
     console.log(`Cambiando estado de la tarea con ID: ${taskId}`);
     try {
-      const task = tasks.find(t => t.id === taskId);
+      const task = tasks.find((t) => t.id === taskId);
       if (!task) {
         console.error(`Tarea con ID ${taskId} no encontrada.`);
         return;
       }
-  
+
       const taskToSend = {
         done: !task.done,
       };
-  
+
       const response = await fetch(`/api/MarcosApiTask/${taskId}`, {
         method: 'PATCH',
         headers: {
@@ -95,19 +98,25 @@ export default function TODO() {
         },
         body: JSON.stringify(taskToSend),
       });
-  
+
       if (response.ok) {
         fetchTasks();
       } else {
-        console.error(`Error al actualizar la tarea ${taskId}:`, response.statusText);
+        console.error(
+          `Error al actualizar la tarea ${taskId}:`,
+          response.statusText
+        );
       }
     } catch (error) {
-      console.error(`Error en la solicitud para actualizar la tarea ${taskId}:`, error);
+      console.error(
+        `Error en la solicitud para actualizar la tarea ${taskId}:`,
+        error
+      );
     }
   }
 
   async function handleDelete(taskId: number) {
-    if (window.confirm("¿Estás seguro de que deseas eliminar esta tarea?")) {
+    if (window.confirm('¿Estás seguro de que deseas eliminar esta tarea?')) {
       try {
         const response = await fetch(`/api/MarcosApiTask/${taskId}`, {
           method: 'DELETE',
@@ -115,47 +124,57 @@ export default function TODO() {
         if (response.ok) {
           fetchTasks();
         } else {
-          console.error(`Error al eliminar la tarea ${taskId}:`, response.statusText);
+          console.error(
+            `Error al eliminar la tarea ${taskId}:`,
+            response.statusText
+          );
         }
       } catch (error) {
-        console.error(`Error en la solicitud para eliminar la tarea ${taskId}:`, error);
+        console.error(
+          `Error en la solicitud para eliminar la tarea ${taskId}:`,
+          error
+        );
       }
     }
   }
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <header className="bg-blue-600 w-full py-4 shadow-md">
-        <h1 className="text-white text-3xl text-center">Mi Aplicación TODO</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+      <header className="w-full bg-blue-600 py-4 shadow-md">
+        <h1 className="text-center text-3xl text-white">Mi Aplicación TODO</h1>
       </header>
 
-      <main className="flex-grow container mx-auto p-4">
-        <div className="bg-white p-6 rounded-lg shadow-lg mb-4">
-          <h2 className="text-2xl font-bold mb-4 text-black">Nueva Tarea</h2>
+      <main className="container mx-auto flex-grow p-4">
+        <div className="mb-4 rounded-lg bg-white p-6 shadow-lg">
+          <h2 className="mb-4 text-2xl font-bold text-black">Nueva Tarea</h2>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
               <FormField
                 control={form.control}
                 name="task"
-                render={({ field }) => (
+                render={({field}) => (
                   <FormItem>
                     <FormControl>
                       <Input placeholder="Tarea a añadir" {...field} />
                     </FormControl>
                     <FormDescription>
                       {submitStatus === 'idle' && (
-                        <div className="bg-yellow-200 p-4 rounded-lg shadow-lg mt-4">
+                        <div className="mt-4 rounded-lg bg-yellow-200 p-4 shadow-lg">
                           <p className="text-yellow-800">Añada su tarea.</p>
                         </div>
                       )}
                       {submitStatus === 'success' && (
-                        <div className="bg-green-200 p-4 rounded-lg shadow-lg mt-4">
-                          <p className="text-green-800">La tarea se ha añadido correctamente.</p>
+                        <div className="mt-4 rounded-lg bg-green-200 p-4 shadow-lg">
+                          <p className="text-green-800">
+                            La tarea se ha añadido correctamente.
+                          </p>
                         </div>
                       )}
                       {submitStatus === 'error' && (
-                        <div className="bg-red-200 p-4 rounded-lg shadow-lg mt-4">
-                          <p className="text-red-800">Error al añadir la tarea.</p>
+                        <div className="mt-4 rounded-lg bg-red-200 p-4 shadow-lg">
+                          <p className="text-red-800">
+                            Error al añadir la tarea.
+                          </p>
                         </div>
                       )}
                     </FormDescription>
@@ -168,18 +187,25 @@ export default function TODO() {
           </Form>
         </div>
 
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h2 className="text-2xl font-bold mb-4 text-black">Lista de Tareas</h2>
+        <div className="rounded-lg bg-white p-6 shadow-lg">
+          <h2 className="mb-4 text-2xl font-bold text-black">
+            Lista de Tareas
+          </h2>
           <ul className="space-y-4">
-          {tasks.map((task) => (
-            <TodoCard key={task.id} task={task} onComplete={handleCheckboxChange} onDelete={handleDelete} />
-          ))}
+            {tasks.map((task) => (
+              <TodoCard
+                key={task.id}
+                task={task}
+                onComplete={handleCheckboxChange}
+                onDelete={handleDelete}
+              />
+            ))}
           </ul>
         </div>
       </main>
 
-      <footer className="bg-blue-600 w-full py-4 mt-4">
-        <p className="text-white text-center">© 2023 Mi Aplicación TODO</p>
+      <footer className="mt-4 w-full bg-blue-600 py-4">
+        <p className="text-center text-white">© 2023 Mi Aplicación TODO</p>
       </footer>
     </div>
   );
