fa709cf74d989c5b5e5cbb46be06a50c79fa9867
MdeMora <miguel.uni.demora@gmail.com>
2024-08-15 12:38:10 +0200

    ZUISO-XXXX: fix migrations issues

--- db/migrations/drizzle/0004_aromatic_misty_knight.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "coffe_table" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"user_id" text NOT NULL,
-	"deMierda" boolean NOT NULL,
-	"cm" integer NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL
-);
