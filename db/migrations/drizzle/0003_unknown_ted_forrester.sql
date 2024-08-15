a538a25bd1067f1c09e2e55fdc44687f75053635
Miguel de Mora <59265254+MdeMora@users.noreply.github.com>
2024-08-15 12:26:13 +0200

    ZUISO test 2 (#31)
    * ZUISO-TEST-2: test migration
    
    * ZUISO-XXXX: testing migration conflicts
    
    * ZUISO-XXXX: testing migration conflicts
    
    * ZUISO-XXXX: testing migration conflicts

---
+++ db/migrations/drizzle/0003_unknown_ted_forrester.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS "motorbikes_table" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"marca" varchar(255) NOT NULL,
+	"modelo" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
