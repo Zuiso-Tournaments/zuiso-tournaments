9681e2624820454c860ba1c083c17a00a0320088
MdeMora <miguel.uni.demora@gmail.com>
2024-08-15 12:06:30 +0200

    ZUISO-TEST-2: test migration

---
+++ db/schemas/motorbikes.ts
@@ -0,0 +1,14 @@
+import { pgTable, varchar,serial, text, timestamp} from 'drizzle-orm/pg-core';
+
+export const carSchema = pgTable('motorbikes_table', {
+  id: serial('id').primaryKey(),
+  marca: varchar('marca', { length: 255 }).notNull(),
+  modelo: text('modelo'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull()
+});
+
+
+export type Bocadillo = typeof carSchema.$inferSelect;
+export type BocadilloInsert = typeof carSchema.$inferInsert;
+
