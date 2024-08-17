93772b65f1f57422e8848fca668b9c74bcf9b308
MdeMora <miguel.uni.demora@gmail.com>
2024-08-17 16:49:30 +0200

    ZUISO-56: config files src

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,8 @@ import {defineConfig} from 'drizzle-kit';
 
 config({path: '.env.local'});
 export default defineConfig({
-  schema: './db/schemas/*.ts',
-  out: './db/migrations/drizzle',
+  schema: './src/db/schemas/*.ts',
+  out: './src/db/migrations/drizzle',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
