d48f851d609242ebc0b2e52e5d732da9d7ddc3bc
cesar muro de zaro jacoste <cesarmurodezarojacoste@macbook-pro-de-cesar.home>
2024-08-18 19:03:26 +0200

    ZUISO-49-BDCESAR

--- app/api/bocadillo/route.ts
+++
@@ -1,8 +0,0 @@
-import {getBocadillo} from '@/actions/bocadillo';
-import {NextResponse} from 'next/server';
-
-export async function GET() {
-  const data = await getBocadillo();
-
-  return NextResponse.json({data});
-}
